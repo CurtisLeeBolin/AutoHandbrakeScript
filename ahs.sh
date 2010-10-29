3b12e13c13d567b868fc2167c9a6bf3f77ab83fb
Curtis Lee Bolin <curtlee@gmail.com>
2010-10-29 21:16:07 +0000

    Simplified FileSearch function.  Added CheckForSubtitles function to now include all subtitles.  Changed CheckForAc3 function to now include all audio tracks.  More work is currently being done to improve the CheckForAc3 function.  It may be currently broke for non-iso files.

--- ahs.sh
+++ ahs.sh
@@ -24,18 +24,22 @@ fileType=( avi flv iso mkv mov mp4 mpeg mpg ogg ogm ogv wmv m2ts rmvb rm 3gp m4a
 readonly DEFAULT_VIDEO_SETTINGS="--encoder x264 --quality 22.5 --decomb --loose-anamorphic"
 readonly DEFAULT_X264_SETTINGS="--x264opts b-adapt=2:rc-lookahead=50"
 readonly DEFAULT_AUDIO_SETTINGS="--audio 1 --aencoder faac --ab 128 --mixdown dpl2 --arate 48 --drc 2.5"
-readonly DEFAULT_SUBTITLE_SETTINGS="--native-language eng --subtitle 1,2,3,4,5,6,7,8,9"
 readonly DEFAULT_CHAPTER_SETTINGS="--markers"
 readonly DEFAULT_CONTAINER_TYPE="mkv"
 readonly DEFAULT_CONTAINER_SETTINGS="--format $DEFAULT_CONTAINER_TYPE"
 readonly DEFAULT_LOG_FILE="handbrake.log"
 readonly DEFAULT_PROCESSED_DIRECTORY="processed"
 readonly DEFAULT_OUTPUT_DIRECTORY="output"
+subtitleSettings=""
 outputDirectory="$DEFAULT_OUTPUT_DIRECTORY"
 otherSettings=""
 mode=""
+audioNumber=""
+encoderCopy=""
 titleOptions=""
+titleCount=""
 fileName=""
+scanList=""
 cropFlag=false
 
 Logger ()
@@ -45,11 +49,25 @@ Logger ()
 
 CheckForAc3 ()
 {
+   audioChannelList=${scanList#*+\ audio\ tracks:}
+   audioChannelList=${audioChannelList%+\ subtitle\ tracks:*}
+   audioChannelCount=`echo $audioChannelList | sed -e 's/+/\n/g' | wc -l`
+   ((audioChannelCount--))
    if [ "${inputFileName##*.}" == "iso" ]
    then
-      audioSettings="--audio 1 --aencoder ac3"
+      audioNumbers="1"
+      encoderCopy="copy"
+      if [ "$audioChannelCount" != "1" ]
+      then
+         for (( i=2; i<="$audioChannelCount"; i++ ))
+         do
+            audioNumbers="$audioNumbers,$i"
+            encoderCopy="$encoderCopy,copy"
+         done
+      fi
+      audioSettings="--audio $audioNumbers --aencoder $encoderCopy"
    else
-      inputAudioCodec=`HandBrakeCLI --scan -i "$inputFileName" 2>&1 | grep "(AC3)"`
+      inputAudioCodec=`HandBrakeCLI --scan --input "$inputFileName" 2>&1 | grep ": Audio: "`
       if [ -z "$inputAudioCodec" ]
       then
          audioSettings="$DEFAULT_AUDIO_SETTINGS"
@@ -59,33 +77,60 @@ CheckForAc3 ()
    fi
 }
 
+CheckForSubtitles ()
+{
+   subtitleChannelList=${scanList#*+\ subtitle\ tracks:}
+   subtitleChannelList=${subtitleChannelList%HandBrake\ has\ exited.*}
+   subtitleChannelCount=`echo $subtitleChannelList | sed -e 's/+/\n/g' | wc -l`
+   if [ "$subtitleChannelCount" == "0" ]
+   then
+      subtitleSettings=""
+   else
+      subtitleNumbers="1"
+      ((subtitleChannelCount--))  # to not count the first blank line
+      if [ "$subtitleChannelCount" != "1" ]
+      then
+         for (( i=2; i<="$subtitleChannelCount"; i++ ))
+         do
+            subtitleNumbers="$subtitleNumbers,$i"
+         done
+      fi
+      subtitleSettings="--subtitle $subtitleNumbers"
+   fi
+}
+
 FileTranscode ()
 {
+   [ "${inputFileName##*.}" != "iso" ] && titleOptions=""
+   scanList=`HandBrakeCLI --scan $titleOptions --input "$DEFAULT_PROCESSED_DIRECTORY"/"$inputFileName" 2>&1`
    CheckForAc3
+   CheckForSubtitles
    Logger "Encoding $inputFileName to $videoName.$DEFAULT_CONTAINER_TYPE ..."
    [ ! -d "$DEFAULT_PROCESSED_DIRECTORY" ] && mkdir -p "$DEFAULT_PROCESSED_DIRECTORY"
    [ ! -d "$outputDirectory" ] && mkdir -p "$outputDirectory"
    mv "$inputFileName" "$DEFAULT_PROCESSED_DIRECTORY"/
    pwd
-   HandBrakeCLI $DEFAULT_VIDEO_SETTINGS $DEFAULT_X264_SETTINGS $audioSettings $DEFAULT_SUBTITLE_SETTINGS $DEFAULT_CONTAINER_SETTINGS $DEFAULT_CHAPTER_SETTINGS $otherSettings --input "$(pwd)"/"$DEFAULT_PROCESSED_DIRECTORY"/"$inputFileName" --output "$(pwd)"/"$outputDirectory"/"$videoName"."$DEFAULT_CONTAINER_TYPE"
+   HandBrakeCLI $DEFAULT_VIDEO_SETTINGS $DEFAULT_X264_SETTINGS $audioSettings $subtitleSettings $DEFAULT_CONTAINER_SETTINGS $DEFAULT_CHAPTER_SETTINGS $otherSettings --input "$(pwd)"/"$DEFAULT_PROCESSED_DIRECTORY"/"$inputFileName" --output "$(pwd)"/"$outputDirectory"/"$videoName"."$DEFAULT_CONTAINER_TYPE"
    Logger "Encoding Completed."
 }
 
 FileSearch ()
 {
-   for inputFileName in *  # stores each file in the directory into inputFileName 1 at a time, then does the loop
+   for inputFileName in *
    do
       if [ -f "$inputFileName" ]  # test if it is a true file
       then
          fileNameExt="${inputFileName##*.}"  # extracts the extension name from the file name
-         predicate="\"$fileType\" = \"$fileNameExt\""                         #
-         for (( i=1; i<${#fileType[@]}; i++ ))                                # creates and list of or's and stores it in predicate
-         do                                                                   #
-            predicate="$predicate -o \"$fileNameExt\" = \"${fileType[i]}\""   #
-         done                                                                 #
-         videoName=${inputFileName%.*}  # extracts the video name from the file name
-         [ $predicate ] && $encodeCommand
-         fi
+         for (( i=0 ; i!=${#fileType[@]} ; i++))
+         do
+            if [ "${fileType[$i]}" == "$fileNameExt" ]
+            then
+               videoName=${inputFileName%.*}  # extracts the video name from the file name
+               $encodeCommand
+               break
+            fi
+         done
+      fi
    done
 }
 
@@ -93,10 +138,13 @@ IsoTranscode ()
 {
    mv "$inputFileName" "$DEFAULT_PROCESSED_DIRECTORY"/
    outputDirectory="${inputFileName%.*}"
-   for (( count=1; count<50; count++ ))
+   titleCount=`HandBrakeCLI --scan --input "$DEFAULT_PROCESSED_DIRECTORY"/"$inputFileName" 2>&1 | grep "scan: DVD has"`
+   titleCount="${titleCount:25}"
+   titleCount="${titleCount%\ title(s)}"
+   for (( count=1; count<="$titleCount"; count++ ))
    do
       [ "$count" -lt "10" ] && number=0$count || number=$count
-      [ $mode == "TitleMode" ] && otherSettings="--title $count" && videoName="Title$number"
+      [ $mode == "TitleMode" ] && titleOptions="--title $count" && otherSettings="$titleOptions" && videoName="Title$number"
       [ $mode == "ChapterMode" ] && otherSettings="--chapters $count $titleOptions" && videoName="Chapter$number"
       FileTranscode
    done
@@ -129,7 +177,7 @@ TitleMode ()
          otherSettings="$titleOptions"
          FileTranscode
       else
-         [ -z titleOptions ] && titleOptions="--main-feature"
+         titleOptions="--main-feature"
          otherSettings="$titleOptions"
          IsoTranscode
       fi
